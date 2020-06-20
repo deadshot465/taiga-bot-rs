b4e8826b29c72654bc30893ba0ca08457c45ca2a
Chehui Chou <deadshot465@users.noreply.github.com>
2020-06-20 15:21:35 +0900

    Add meal command.

--- src/commands/meal.rs
+++ src/commands/meal.rs
@@ -1,5 +1,24 @@
+use serde::{Deserialize, Serialize};
 use serenity::framework::standard::{macros::{
     command
 }, CommandResult};
 use serenity::prelude::Context;
 use serenity::model::channel::Message;
+
+#[derive(Deserialize, Serialize)]
+struct Meal {
+
+}
+
+#[command]
+pub async fn meal(context: &Context, msg: &Message) -> CommandResult {
+    let client = reqwest::Client::new();
+    let response = client.get("http://www.themealdb.com/api/json/v1/1/random.php")
+        .send()
+        .await?
+        .json()
+        .await?;
+    let color = u32::from_str_radix("fd9b3b", 16).unwrap();
+
+    Ok(())
+}
